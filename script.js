b41e4081c7823434dd0e4c1809809c7c30a355f6
GabrielAzamfiri <167236687+GabrielAzamfiri@users.noreply.github.com>
2024-06-12 18:10:58 +0200

    add commenti sul lavoro svolto in js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let carrello = [];
+
+// *************mi prendo la ali tramite fetch************************
 fetch("https://striveschool-api.herokuapp.com/books")
   .then((responseObj) => {
+    // *************risposta************************
     console.log(responseObj);
     if (responseObj.ok) {
       return responseObj.json();
     } else {
       throw new Error("Errore nel reperimento dei dati");
     }
-  })
+  }) // *************array della risposta****************************************
   .then((arrayBooks) => {
-    console.log(arrayBooks);
+    // *************creo la card book**************************************************
     const row = document.getElementById("card-container");
     arrayBooks.forEach((book) => {
       const col = document.createElement("div");
@@ -34,6 +37,7 @@ fetch("https://striveschool-api.herokuapp.com/books")
       cardText.classList.add("card-text");
       cardText.textContent = book.price + "$";
 
+      // *************creo btn delete********************************************
       const btnDelete = document.createElement("button");
       btnDelete.classList.add("btn");
       btnDelete.classList.add("btn-danger");
@@ -43,11 +47,13 @@ fetch("https://striveschool-api.herokuapp.com/books")
         col.remove();
       });
 
+      // *************creo btn addToCart**********************************************
+
       const addToCart = document.createElement("button");
       addToCart.classList.add("btn");
       addToCart.classList.add("btn-primary");
       addToCart.textContent = "Add To Cart";
-
+      // *************addEventListener sul bottone addToCart********************************************
       addToCart.addEventListener("click", () => {
         const cart = document.getElementById("cart");
         cart.classList.add("row");
@@ -56,15 +62,19 @@ fetch("https://striveschool-api.herokuapp.com/books")
         cartItem.classList.add("list-group-item");
         cartItem.classList.add("my-2");
         cartItem.textContent = book.title;
+
+        // **********************creo btnDeleteCart******************
         const btnDeleteCart = document.createElement("button");
         btnDeleteCart.classList.add("btn");
         btnDeleteCart.classList.add("btn-danger");
         btnDeleteCart.classList.add("ms-5");
-
         btnDeleteCart.textContent = "Delete Book";
+
+        // *************addEventListener sul bottone btnDeleteCart********************************************
         btnDeleteCart.addEventListener("click", () => {
           cartItem.remove();
           carrello.splice(carrello.indexOf(book.title), 1);
+          // dopo aver tolto il libro dal array carrello rifaccio lo storage del carrello aggioranto
           localStorage.setItem("carrello", JSON.stringify(carrello));
         });
         carrello.push(book.title);
@@ -81,12 +91,20 @@ fetch("https://striveschool-api.herokuapp.com/books")
   })
   .catch((err) => console.log(err));
 
+// *************addEventListener al caricamento della pagina********************************************
+
 window.addEventListener("DOMContentLoaded", () => {
   const storedCarrello = localStorage.getItem("carrello");
+  // se la key carrello esiste nello storage
   if (storedCarrello) {
-    // se ho trovato il nome saremo qui dentro
+    // faccio tornare un array il mio dato nello storage
     const arrCarrello = JSON.parse(storedCarrello);
+
+    // al caricamento della pagina riscrivo anche il mio carrello cosi non parte da vuoto
+    // serve per far si che ad un nuovo addToCart continui ad aggiungere senza ripartire da vuoto
     carrello = arrCarrello;
+
+    // per ogni item nel carrello ricrea le varie li con btnDeleteCart che ha ancora l'eventListener al click
     arrCarrello.forEach((item) => {
       const cart = document.getElementById("cart");
       cart.classList.add("row");
